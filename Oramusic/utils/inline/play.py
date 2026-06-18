e0b7fe209e4e5c32525560744a8d8871ad9c3819
Jani-Ka-Repo <santidevi654378@gmail.com>
2026-06-19 01:35:51 +0530

    Update play.py

--- Oramusic/utils/inline/play.py
+++ Oramusic/utils/inline/play.py
@@ -59,13 +59,13 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [   
-             InlineKeyboardButton(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
-             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+      #  [   
+            # InlineKeyboardButton(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
+             #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            # InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+             #InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            # InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        #],
     ]
     
     return buttons
@@ -73,13 +73,13 @@ def stream_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-             InlineKeyboardButton(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
-             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+       # [
+             #InlineKeyboardButton(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
+             #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+           #  InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            # InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+             #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        #],
     ]
     return buttons
 
